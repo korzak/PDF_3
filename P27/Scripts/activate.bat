307842ed3baa18b41c49f68002250899c125fdf7
Aleksandr <a.korbut72@gmail.com>
2019-03-29 21:52:27 +0300

    окружение

---
+++ P27/Scripts/activate.bat
@@ -0,0 +1,26 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\User\YandexDisk\repos\PDF_Psrder_2_git\PythonApplication1\P27"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+	set "_OLD_VIRTUAL_PROMPT=%PROMPT%"	
+)
+set "PROMPT=(P27) %PROMPT%"
+
+if not defined _OLD_VIRTUAL_PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+)
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
